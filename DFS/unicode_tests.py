85138505fde8a6777253e99ac0e399980048bd9d
Guillaume Roux <guillaume@tourelle.guillaumeroux.fr>
2019-08-08 01:19:01 +0200

    DFS fonctionne

--- DFS/unicode_tests.py
+++ DFS/unicode_tests.py
@@ -4,29 +4,28 @@ import random
 import os
 """
 La convention est la suivante:
- - u pour le demi-trait du haut
- - r pour le demi-trait de droite
- - d pour le demi-trait du bas
- - l pour le demi-trait de gauche
+ - 0001 pour le demi-trait du haut
+ - 0010 pour le demi-trait de droite
+ - 0100 pour le demi-trait du bas
+ - 1000 pour le demi-trait de gauche
 """
-
-codes = {'lr':   0x2500,
-         'du':   0x2502,
-         'dr':   0x250C,
-         'dl':   0x2510,
-         'ru':   0x2514,
-         'lu':   0x2518,
-         'dru':  0x251C,
-         'dlu':  0x2524,
-         'dlr':  0x252C,
-         'lru':  0x2534,
-         'dlru': 0x253C,
-         'l':    0x2574,
-         'u':    0X2575,
-         'r':    0x2576,
-         'd':    0x2577,
-         ' ':    0x20}
-
+codes = [  0x20, # 0000 vide
+         0x2575, # 0001 up
+         0x2576, # 0010 right
+         0x2514, # 0011 
+         0x2577, # 0100 
+         0x2502, # 
+         0x250c,
+         0x251c,
+         0x2574,
+         0x2518,
+         0x2500,
+         0x2534,
+         0x2510,
+         0x2524,
+         0x252c,
+         0x253c,
+]
 def grille_aleatoire(largeur,hauteur):
     for i in range(hauteur):
         ligne=''
@@ -40,7 +39,7 @@ def creer_matrice(largeur,hauteur):
     for i in range(hauteur):
         matrice.append([])
         for j in range(largeur):
-            matrice[-1].append(' ')
+            matrice[-1].append(0)
             #matrice[-1].append(0x20)
             #matrice[-1].append(chr(codes[random.choice(list(codes))]))
     return matrice
@@ -64,36 +63,62 @@ def generer_profondeur(matrice):
     une_pile.append((i_0, j_0))
 
     while une_pile:
-        i, j = une_pile.pop()
+        i, j = une_pile[-1]
 
-        voisins_occupes = matrice[i][j]
-        voisins_libres = ''
+        #voisins_libres = 0
         
-        if u_libre(matrice,i,j): voisins_libres += 'u'
-        if r_libre(matrice,i,j): voisins_libres += 'r'
-        if d_libre(matrice,i,j): voisins_libres += 'd'
-        if l_libre(matrice,i,j): voisins_libres += 'l'
+        #if u_libre(matrice,i,j): voisins_libres |= 1
+        #if r_libre(matrice,i,j): voisins_libres |= 2
+        #if d_libre(matrice,i,j): voisins_libres |= 4
+        #if l_libre(matrice,i,j): voisins_libres |= 8
 
-        print('voisins_libres', voisins_libres)
-        while voisins_libres:
-            direction = random.choice(voisins_libres)
-            voisins_libres = voisins_libres.replace(direction,'')
-            
-            une_pile.append(char_en_couple(direction,i,j))
-            matrice[i][j] = ''.join(sorted(matrice[i][j] + direction)).strip()
+        voisins_libres = []
+        
+        if u_libre(matrice,i,j): voisins_libres.append((i-1,j))
+        if r_libre(matrice,i,j): voisins_libres.append((i,j+1))
+        if d_libre(matrice,i,j): voisins_libres.append((i+1,j))
+        if l_libre(matrice,i,j): voisins_libres.append((i,j-1))
 
+        #if u_libre(matrice,i,j): voisins_libres.append(1)
+        #if r_libre(matrice,i,j): voisins_libres.append(2)
+        #if d_libre(matrice,i,j): voisins_libres.append(4)
+        #if l_libre(matrice,i,j): voisins_libres.append(8)
+
+        random.shuffle(voisins_libres)
+        
+        #print('voisins_libres', voisins_libres)
+
+        if voisins_libres:
+            I, J = voisins_libres.pop()
+            if (I,J) == (i-1,j):
+                matrice[i][j] |= 1
+                matrice[I][J] |= 4
+            if (I,J) == (i,j+1):
+                matrice[i][j] |= 2
+                matrice[I][J] |= 8
+            if (I,J) == (i+1,j):
+                matrice[i][j] |= 4
+                matrice[I][J] |= 1
+            if (I,J) == (i,j-1):
+                matrice[i][j] |= 8
+                matrice[I][J] |= 2
+                
+            une_pile.append((I,J))
+
+            afficher_matrice(matrice)
             print('matrice[i][j]:', matrice[i][j], 'len(une_pile):', len(une_pile))
             input()
-            afficher_matrice(matrice)
-        
+        else:
+            une_pile.pop()
+            
 def u_libre(matrice,i,j):
-    return i > 0 and matrice[i-1][j] == ' '
+    return i > 0 and matrice[i-1][j] == 0
 def r_libre(matrice,i,j):
-    return j < len(matrice[0])-1 and matrice[i][j+1] == ' '
+    return j < len(matrice[0])-1 and matrice[i][j+1] == 0
 def d_libre(matrice,i,j):
-    return i < len(matrice)-1 and matrice[i+1][j] == ' '
+    return i < len(matrice)-1 and matrice[i+1][j] == 0
 def l_libre(matrice,i,j):
-    return j > 0 and matrice[i][j-1] == ' '
+    return j > 0 and matrice[i][j-1] == 0
 
 def char_en_couple(c,i,j):
     if c == 'u': return (i-1,j)
@@ -103,12 +128,8 @@ def char_en_couple(c,i,j):
 
 if __name__ == '__main__':
 
-    for clef in codes:
-        code = codes[clef]
-        print(hex(code),code,chr(code))
-
     #grille_aleatoire(100,15)
-    matrice = creer_matrice(100,15)
+    matrice = creer_matrice(60,30)
     afficher_matrice(matrice)
 
     generer_profondeur(matrice)
